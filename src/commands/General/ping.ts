22e22e533b6f13b8b673005d5a8e1743b7ea4e8a
Mieko Hikari <Mieko.Hikarii@gmail.com>
2023-06-14 10:02:08 +0400

    Making ping command more user friendly 💗
    Created an embed for the command!

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,10 +1,9 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
-import { send } from '@sapphire/plugin-editable-commands';
-import { ApplicationCommandType, type Message } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
-	description: 'ping pong'
+	description: 'Amari Bot to Discord: Message travel time'
 })
 export class UserCommand extends Command {
 	// Register slash and context menu command
@@ -14,52 +13,26 @@ export class UserCommand extends Command {
 			name: this.name,
 			description: this.description
 		});
-
-		// Register context menu command available from any message
-		registry.registerContextMenuCommand({
-			name: this.name,
-			type: ApplicationCommandType.Message
-		});
-
-		// Register context menu command available from any user
-		registry.registerContextMenuCommand({
-			name: this.name,
-			type: ApplicationCommandType.User
-		});
 	}
 
-	// Message command
-	public async messageRun(message: Message) {
-		const msg = await send(message, 'Ping?');
-
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)
-		}ms.`;
-
-		return send(message, content);
-	}
 	// slash command
 	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const msg = await interaction.reply({ content: 'Ping?', fetchReply: true });
+		const msg = await interaction.reply({ content: 'Ping?', fetchReply: true, ephemeral: true });
 
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			msg.createdTimestamp - interaction.createdTimestamp
-		}ms.`;
-
-		return await interaction.editReply({
-			content: content
-		});
-	}
-	// context menu command
-	public async contextMenuRun(interaction: Command.ContextMenuCommandInteraction) {
-		const msg = await interaction.reply({ content: 'Ping?', fetchReply: true });
-
-		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${
-			msg.createdTimestamp - interaction.createdTimestamp
-		}ms.`;
+		const embed = new EmbedBuilder()
+			.setAuthor({ name: '/ping', iconURL: interaction.client.user.displayAvatarURL() })
+			.setColor('Blurple')
+			.setTitle('OH YEAH I\'VE BEEN PINGED')
+			.setDescription('Hey there! This is how long it took me to realize you pinged me 😅')
+			.setFields(
+				{ name: 'Bot Latency:', value: `${this.container.client.ws.ping}ms`, inline: true },
+				{ name: 'API Latency:', value: `${msg.createdTimestamp - interaction.createdTimestamp}ms`, inline: true }
+			)
+			.setTimestamp();
 
 		return await interaction.editReply({
-			content: content
+			content: '',
+			embeds: [embed],
 		});
 	}
 }
