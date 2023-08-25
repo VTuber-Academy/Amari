6f7d11504dc9185a6966aa3590af1a73fec8fbc9
Mieko Hikari <Mieko.Hikarii@gmail.com>
2023-08-25 09:59:14 +0400

    Captcha now part of server security

--- src/Amari Levels/lib/levelDataBase.ts
+++ src/Amari Levels/lib/levelDataBase.ts
@@ -11,7 +11,7 @@ const levelSchema = new Schema<LevelsInterface>({
 	id: { type: String, required: true },
 	level: { type: Number, default: 0 },
 	experience: { type: Number, default: 0 },
-	lastActivity: { type: Number, default: new Date() }
+	lastActivity: { type: Date, default: new Date() }
 });
 
 const levelDatabase = model<LevelsInterface>('Amari Levels', levelSchema);
