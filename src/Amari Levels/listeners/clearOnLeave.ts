7b3a48b96e77fcded31a123c9af296b03473611f
Mieko Hikari <Mieko.Hikarii@gmail.com>
2023-10-08 08:36:48 +0400

    Bufgix

--- src/Amari Levels/listeners/clearOnLeave.ts
+++ src/Amari Levels/listeners/clearOnLeave.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener } from '@sapphire/framework';
-import levelDatabase from 'Amari Levels/lib/levelDataBase';
+import levelDatabase from '../lib/levelDataBase';
 import { GuildMember } from 'discord.js';
 
 @ApplyOptions<Listener.Options>({
