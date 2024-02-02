6bdcbe1af720d413e6ba280eb5d6b6858389e47d
Mieko Hikari <Mieko.Hikarii@gmail.com>
2024-02-02 14:21:38 +0400

    ditch old tracker
    New tracker utilizes NodeJS Timers to register minutes into the tracker so that we have more control over how they're earning xp to prevent further abuse.

--- src/Amari Levels/listeners/voiceChannelTracker.ts
+++ src/Amari Levels/listeners/voiceChannelTracker.ts
@@ -1,12 +1,10 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener } from '@sapphire/framework';
 import { EmbedBuilder, type VoiceState } from 'discord.js';
-import { Stopwatch } from '@sapphire/stopwatch';
-import { Duration } from '@sapphire/time-utilities';
 import levelManager from '../lib/levelManager';
 import config from '../config.json';
 
-const trackerMap = new Map<string, Stopwatch>();
+const trackerMap = new Map<string, { activeMinutes: number; intervalId: NodeJS.Timer }>();
 
 @ApplyOptions<Listener.Options>({
 	event: Events.VoiceStateUpdate,
@@ -29,26 +27,37 @@ export class UserEvent extends Listener {
 
 		if (!oldState.channelId && newState.channelId) {
 			if (!newState.member) return;
-			trackerMap.set(newState.member.id, new Stopwatch());
+
+			const intervalId: NodeJS.Timer = setInterval(() => {
+				if (!newState.member) return clearInterval(intervalId);
+
+				const tracker = trackerMap.get(newState.member.id);
+				if (!tracker) return clearInterval(intervalId);
+
+				if (newState.member.voice.selfMute) {
+					tracker.activeMinutes += 0.5;
+				} else if (newState.member.voice.streaming) {
+					tracker.activeMinutes += 1.2;
+				} else {
+					tracker.activeMinutes += 1;
+				}
+			}, 60000);
+
+			trackerMap.set(newState.member.id, { activeMinutes: 0, intervalId });
 		} else if (oldState.channelId && !newState.channelId) {
 			if (!oldState.member) return;
 
-			const stopwatch = trackerMap.get(oldState.member.id);
-			if (!stopwatch) return;
-			stopwatch.stop();
-
-			trackerMap.delete(oldState.member.id);
-
-			const minutes = Math.floor(new Duration(stopwatch.toString()).seconds / 60);
+			const tracker = trackerMap.get(oldState.member.id);
+			if (!tracker) return;
 
 			let xp = 0;
 
-			for (let minute = 0; minute < minutes; minute++) {
-				xp += Math.floor(Math.random() * (5 - 2.5 + 1)) + 2.5;
+			for (let minute = 0; minute < tracker.activeMinutes; minute++) {
+				xp += Math.floor(Math.random() * 8) * ((minute + 1) / (tracker.activeMinutes + 1));
 			}
 
 			const msg = await oldState.channel?.send(
-				`**${oldState.member.displayName}** has gained **${xp}** XP for being in the voice channel for **${minutes}** minutes!`
+				`**${oldState.member.displayName}** has gained **${xp}** XP for being in the voice channel for **${tracker.activeMinutes}** minutes!`
 			);
 
 			levelManager.addXP(xp, oldState.member.id).then(async (levelled) => {
@@ -56,6 +65,9 @@ export class UserEvent extends Listener {
 					await msg?.react('⭐');
 				}
 			});
+
+			clearInterval(tracker.intervalId);
+			trackerMap.delete(oldState.member.id);
 		}
 
 		return;
