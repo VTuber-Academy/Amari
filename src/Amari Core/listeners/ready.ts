c7a4070969d904a2d0944746c24df72f62e572e4
Mieko Hikari <Mieko.Hikarii@gmail.com>
2024-02-07 20:54:34 +0400

    Remove scuffed version command

--- src/Amari Core/listeners/ready.ts
+++ src/Amari Core/listeners/ready.ts
@@ -2,10 +2,8 @@ import { ApplyOptions } from '@sapphire/decorators';
 import { Listener, type Store } from '@sapphire/framework';
 import { blue, gray, green, magenta, magentaBright, white, yellow } from 'colorette';
 import mongoose from 'mongoose';
-import { Octokit } from '@octokit/rest';
 
 const dev = process.env.NODE_ENV !== 'production';
-let version = 'fetching...';
 
 @ApplyOptions<Listener.Options>({ once: true })
 export class UserEvent extends Listener {
@@ -16,10 +14,6 @@ export class UserEvent extends Listener {
 		this.printStoreDebugInformation();
 
 		await this.connectMongo();
-		await this.fetchVersion().then((v) => {
-			version = v;
-			this.container.logger.info(`Version retrieved! Amari is running on  #${version}`);
-		});
 	}
 
 	private async connectMongo() {
@@ -31,20 +25,6 @@ export class UserEvent extends Listener {
 			.connect(process.env.MongoDB_URL, { dbName: process.env.MongoDB_Name })
 			.catch((error) => new Error(error))
 			.then(() => this.container.logger.info(`Mongoose successfully connected!`));
-	}
-
-	private async fetchVersion() {
-		const octokit = new Octokit({
-			auth: process.env.GITHUB_TOKEN
-		});
-
-		const { data } = await octokit.rest.repos.listCommits({
-			owner: process.env.GITHUB_ORG ?? '',
-			repo: process.env.GITHUB_REPO ?? ''
-		});
-
-		const latestCommitHash = data[0].sha.substring(0, 7);
-		return latestCommitHash;
 	}
 
 	private printBanner() {
@@ -82,5 +62,3 @@ ${line03}${dev ? ` ${pad}${blc('<')}${llc('/')}${blc('>')} ${llc('DEVELOPMENT MO
 		return gray(`${last ? '└─' : '├─'} Loaded ${this.style(store.size.toString().padEnd(3, ' '))} ${store.name}.`);
 	}
 }
-
-export default version;
