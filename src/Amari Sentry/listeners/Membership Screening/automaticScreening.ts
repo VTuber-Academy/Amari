43a3b2c7edc398272941706f86fa55270f4f588a
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-04-08 05:45:13 +0400

    Fix CompletedOnboarding flag check in automatic screening

--- src/Amari Sentry/listeners/Membership Screening/automaticScreening.ts
+++ src/Amari Sentry/listeners/Membership Screening/automaticScreening.ts
@@ -42,7 +42,7 @@ export class UserEvent extends Listener {
 			newMember.client.logger.debug(`[Sentry] Kicked ${newMember.user.username}|${newMember.id} for removing consent`);
 		}
 
-		if (oldMember.flags.missing('CompletedOnboarding') && newMember.flags.has('CompletedOnboarding')) {
+		if (!oldMember.flags.has('CompletedOnboarding') && newMember.flags.has('CompletedOnboarding')) {
 			this.screenMember(newMember);
 		}
 	}
